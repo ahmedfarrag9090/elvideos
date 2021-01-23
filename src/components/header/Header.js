9aba44eea226c63a967499d37b7a2efa5e664e56
ahmedfarrag9090 <farrag9090@gmail.com>
2021-01-23 19:15:36 +0200

    init

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,10 +4,9 @@ import requests from "../../requests";
 import "./Header.css";
 
 const Header = () => {
-  
   const [movie, setMovie] = useState(null);
 
-  //  fetch rabdom movie to render backdrop image and title when component did mount 
+  //  fetch rabdom movie to render backdrop image and title when component did mount
   useEffect(() => {
     async function fetchRandomMovie() {
       const request = await axiosInstance.get(requests.fetchActionMovies);
@@ -20,34 +19,40 @@ const Header = () => {
     fetchRandomMovie();
   }, []);
 
+  
 
   //  to show 3 dots in too long texts
-  const truncateText= (txt, n) => {
-      return txt?.length > n ?txt.substr(0, n - 1) + "..." : txt;
-  }
+  const truncateText = (txt, n) => {
+    return txt?.length > n ? txt.substr(0, n - 1) + "..." : txt;
+  };
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url(https://image.tmdb.org/t/p/w1280${
-          movie?.backdrop_path || movie?.poster_path})`,
-        backgroundPosition: "center center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="banner__content">
-        <h1 className="banner__title">
-          {movie?.title || movie?.original_title || movie?.name}
-        </h1>
-        <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
+
+   return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/w1280${
+            movie?.backdrop_path || movie?.poster_path
+          })`,
+          backgroundPosition: "center center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__content">
+          <h1 className="banner__title">
+            {movie?.title || movie?.original_title || movie?.name}
+          </h1>
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+          <h4 className="banner__overview">
+            {truncateText(movie?.overview, 150)}
+          </h4>
         </div>
-        <h4 className="banner__overview">{truncateText(movie?.overview, 150)}</h4>
-      </div>
-    </header>
-  );
+      </header>
+    );
+
 };
 
 export default Header;
