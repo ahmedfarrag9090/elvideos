574fa07774c61effd09fd3c2fd0b5272b05aed5d
ahmedfarrag9090 <farrag9090@gmail.com>
2021-02-21 06:43:46 +0200

    some edit

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
         <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
         <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
         <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
-        {/* <Row title="Romantic" fetchURL={requests.fetchRomanticMovies} /> */}
-        {/* <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} /> */}
+        <Row title="Romantic" fetchURL={requests.fetchRomanticMovies} />
+        <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
       </div>
     </div>
   ); 
